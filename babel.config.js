9734beca8fab7eb49bcf6a7386f5f2d91fb4e51d
musayevnadir <msyv.nadir@gmail.com>
2024-04-19 15:53:05 +0400

    Added Absolute Paths in React Native

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,39 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.ts',
+          '.android.ts',
+          '.ts',
+          '.ios.tsx',
+          '.android.tsx',
+          '.tsx',
+          '.jsx',
+          '.js',
+          '.json',
+          '.png',
+        ],
+        alias: {
+          assets: './src/assets',
+          components: './src/components',
+          configs: './src/configs',
+          hooks: './src/hooks',
+          router: './src/router',
+          helpers: './src/helpers',
+          screens: './src/screens',
+          services: './src/services',
+          store: './src/store',
+          theme: './src/theme',
+          types: './src/types',
+          constants: './src/constants',
+          utils: './src/utils',
+        },
+      },
+    ],
+    // 'react-native-reanimated/plugin',
+  ],
 };
