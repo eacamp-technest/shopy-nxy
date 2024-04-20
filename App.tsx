2564e5a29586ae6f9b6898eaf47a1d2cd708c833
musayevnadir <msyv.nadir@gmail.com>
2024-04-20 22:10:59 +0400

    fix:Updated TypographyStyles

--- App.tsx
+++ App.tsx
@@ -2,10 +2,29 @@ import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SvgImage} from 'components/SvgImage';
 import {ImageResources} from 'assets/VectorResources.g';
+import {TypographyStyles} from 'theme/typography';
 
 const App = () => {
   return (
     <View style={styles.container}>
+      <Text style={TypographyStyles.title1}>
+        This text uses a thin italic raleway font
+      </Text>
+      <Text style={TypographyStyles.title2}>
+        This text uses a thin italic raleway font
+      </Text>
+      <Text style={TypographyStyles.title3}>
+        This text uses a thin italic raleway font
+      </Text>
+      <Text style={TypographyStyles.smallRegular}>
+        This text uses a thin italic raleway font
+      </Text>
+      <Text style={styles.ralewayItalicBebasReg}>
+        This text uses a thin italic raleway font
+      </Text>
+      <Text style={styles.ralewayItalicBebasLigh}>
+        This text uses a thin italic raleway font
+      </Text>
       <Text style={styles.ralewayItalicBebas}>
         This text uses a thin italic raleway font
       </Text>
--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -21,7 +21,6 @@ export const commonFontStyling: TextStyle = {
 const fontSize48 = normalize('font', 48);
 const fontSize32 = normalize('font', 32);
 const fontSize24 = normalize('font', 24);
-const fontSize18 = normalize('font', 18);
 const fontSize16 = normalize('font', 16);
 const fontSize14 = normalize('font', 14);
 const fontSize12 = normalize('font', 12);
@@ -31,7 +30,6 @@ const lineHeight36 = normalize('font', 36);
 const lineHeight32 = normalize('font', 32);
 const lineHeight24 = normalize('font', 24);
 const lineHeight20 = normalize('font', 20);
-const lineHeight18 = normalize('font', 18);
 const lineHeight16 = normalize('font', 16);
 const lineHeight14 = normalize('font', 14);
 const lineHeight12 = normalize('font', 12);
@@ -55,76 +53,54 @@ export const TypographyStyles = StyleSheet.create({
     lineHeight: lineHeight32,
     ...commonFontStyling,
   } as TextStyle,
-  regularNone: {
+  smallNormal: {
+    fontFamily: montseratFonts[400],
+    fontSize: fontSize12,
+    lineHeight: lineHeight16,
+  },
+  mediumNormal: {
+    fontFamily: montseratFonts[400],
+    fontSize: fontSize14,
+    lineHeight: lineHeight16,
+  },
+  mediumLarge: {
+    fontFamily: montseratFonts[400],
+    fontSize: fontSize14,
+    lineHeight: lineHeight20,
+  },
+  normalNormal: {
     fontFamily: montseratFonts[400],
     fontSize: fontSize16,
     lineHeight: lineHeight16,
-    ...commonFontStyling,
-  } as TextStyle,
-  regularTight: {
-    fontFamily: montseratFonts[600],
+  },
+  normalLarge: {
+    fontFamily: montseratFonts[400],
     fontSize: fontSize16,
     lineHeight: lineHeight20,
-    ...commonFontStyling,
-  } as TextStyle,
-  regularNormal: {
-    fontFamily: montseratFonts[700],
+  },
+  normalXLarge: {
+    fontFamily: montseratFonts[400],
     fontSize: fontSize16,
     lineHeight: lineHeight24,
-    ...commonFontStyling,
-  } as TextStyle,
-  smallNone: {
-    fontFamily: montseratFonts[400],
-    fontSize: fontSize14,
-    lineHeight: lineHeight14,
-    ...commonFontStyling,
-  } as TextStyle,
-  smallTight: {
+  },
+  smallSemiBold: {
     fontFamily: montseratFonts[600],
-    fontSize: fontSize14,
-    lineHeight: lineHeight16,
-    ...commonFontStyling,
-  } as TextStyle,
-  smallNormal: {
-    fontFamily: montseratFonts[700],
-    fontSize: fontSize14,
-    lineHeight: lineHeight20,
-    ...commonFontStyling,
-  } as TextStyle,
-  tinyNone: {
-    fontFamily: montseratFonts[400],
     fontSize: fontSize12,
     lineHeight: lineHeight12,
-    ...commonFontStyling,
-  } as TextStyle,
-  tinyTight: {
+  },
+  mediumSemiBold: {
     fontFamily: montseratFonts[600],
-    fontSize: fontSize12,
+    fontSize: fontSize14,
     lineHeight: lineHeight14,
-    ...commonFontStyling,
-  } as TextStyle,
-  tinyNormal: {
-    fontFamily: montseratFonts[700],
-    fontSize: fontSize12,
-    lineHeight: lineHeight16,
-    ...commonFontStyling,
-  } as TextStyle,
-  largeNone: {
-    fontFamily: montseratFonts[400],
-    fontSize: fontSize18,
-    lineHeight: lineHeight18,
-    ...commonFontStyling,
-  } as TextStyle,
-  largeTight: {
+  },
+  normalSemiBold: {
     fontFamily: montseratFonts[600],
-    fontSize: fontSize18,
+    fontSize: fontSize16,
+    lineHeight: lineHeight16,
+  },
+  largeSemiBold: {
+    fontFamily: montseratFonts[600],
+    fontSize: fontSize16,
     lineHeight: lineHeight20,
-    ...commonFontStyling,
-  } as TextStyle,
-  largeNormal: {
-    fontFamily: montseratFonts[700],
-    fontSize: fontSize18,
-    lineHeight: lineHeight24,
-    ...commonFontStyling,
-  } as TextStyle,
+  },
 });
