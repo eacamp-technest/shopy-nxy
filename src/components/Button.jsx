76b67a42af895622a361b0ad6be10294da98fd5f
musayevnadir <msyv.nadir@gmail.com>
2024-04-24 20:07:28 +0400

    feature:create common-styles

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,21 @@
-import { StyleSheet, View, Text } from 'react-native';
-import { colors } from 'theme/colors';
-import { fonts } from 'assets/fonts';
+import {StyleSheet, View, Text} from 'react-native';
+import {colors} from 'theme/colors';
+import {fonts} from 'assets/fonts';
 
 const styles = StyleSheet.create({
   button: {
     backgroundColor: colors.primary.main,
     width: 327,
-    paddingTop:16,
+    paddingTop: 16,
     borderRadius: 8,
     alignItems: 'center',
   },
   text: {
-    fontFamily: fonts.Montserrat-Regular,
+    fontFamily: fonts.Montserrat - Regular,
     fontSize: 16,
     fontWeight: '600',
     lineHeight: 16,
-  }
+  },
 });
 
 return (
