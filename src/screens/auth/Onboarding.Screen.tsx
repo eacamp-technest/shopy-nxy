4f7f20c388eb42b59fa3dee5edf5247cce8dc844
Elnur <elnur@Elnur-MacBook-Air.local>
2024-04-30 06:57:13 -0700

    feature:bugs fixed

--- src/screens/auth/Onboarding.Screen.tsx
+++ src/screens/auth/Onboarding.Screen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import {Button} from 'components/Button';
+import {RadioButton} from 'components/RadioButton';
 
 export const OnboardingScreen = () => {
   return (
     <View style={styles.root}>
       <Button position={'center'} text={'Create'} type={'secondary'} />
+      <RadioButton />
     </View>
   );
 };
