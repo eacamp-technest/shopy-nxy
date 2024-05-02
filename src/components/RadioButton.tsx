5db4c086f8fde2acd7ac14b1a3720761f1856f82
musayevnadir <msyv.nadir@gmail.com>
2024-05-02 22:18:30 +0400

    fix:radio-button

--- src/components/RadioButton.tsx
+++ src/components/RadioButton.tsx
@@ -10,65 +10,55 @@ interface IRadioButton {
   disabled?: boolean;
   onPress?: () => void;
 }
-export const RadioButton = ({disabled}: IRadioButton) => {
+
+export const RadioButton: React.FC<IRadioButton> = ({disabled}) => {
   const [checkedState, setCheckedState] = useState<boolean>(false);
-  const getChangedIcon = (check: boolean) => {
-    setCheckedState(check);
-  };
+
+  const handleRadioButton = () => setCheckedState(!checkedState);
 
   return (
     <Pressable
       hitSlop={standardHitSlopSize}
-      onPress={() => getChangedIcon(!checkedState)}
+      onPress={handleRadioButton}
+      disabled={disabled}
       style={[
-        styles.button,
-        disabled
-          ? styles.disabled
-          : checkedState
-          ? styles.checkOn
-          : styles.normal,
+        styles.normal,
+        checkedState && styles.press,
+        disabled && styles.disabledNormal,
       ]}>
-      <View style={[disabled ? styles.disableCircle : styles.circle]} />
+      <View style={[styles.pressVariant, disabled && styles.disabled]} />
     </Pressable>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
-    borderColor: colors.skyBase,
+  normal: {
+    borderWidth: 1,
     borderRadius: 32,
+    borderColor: colors.skyBase,
     height: normalize('height', 24),
     width: normalize('width', 24),
     ...CommonStyles.alignJustifyCenter,
   },
-  normal: {
-    borderWidth: 1,
+
+  disabledNormal: {
+    borderWidth: 0,
+    backgroundColor: colors.skyLight,
   },
-  checkOn: {
-    padding: 8,
+
+  press: {
+    borderWidth: 0,
     backgroundColor: colors.primary.base,
   },
-  circle: {
-    borderWidth: 1,
+
+  pressVariant: {
     borderRadius: 32,
-    borderColor: colors.white,
-    height: normalize('height', 8),
-    width: normalize('width', 8),
     backgroundColor: colors.white,
-  },
-  disabled: {
-    borderRadius: 32,
-    backgroundColor: colors.skyLight,
-    borderColor: colors.skyLight,
-    height: normalize('height', 24),
-    width: normalize('width', 24),
-  },
-  disableCircle: {
-    borderWidth: 1,
-    borderRadius: 32,
     height: normalize('height', 8),
     width: normalize('width', 8),
-    borderColor: colors.skyLighter,
+  },
+
+  disabled: {
     backgroundColor: colors.skyLighter,
   },
 });
