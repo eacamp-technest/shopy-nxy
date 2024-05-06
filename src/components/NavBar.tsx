1585ed9410276a93413c37ce7065ab3931b5b6eb
musayevnadir <msyv.nadir@gmail.com>
2024-05-06 20:49:05 +0400

    feature:nav-bar

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,67 @@
+import React, {Fragment} from 'react';
+import {View, Text, Pressable, ViewStyle, StyleProp} from 'react-native';
+import {TypographyStyles} from 'theme/typography';
+import {SvgImage} from './SvgImage';
+import {standardHitSlopSize} from 'theme/consts.styles';
+import {CommonStyles} from 'theme/commonStyles';
+import {Resources} from './SvgImage';
+
+// ! Interface
+
+interface INavBar {
+  title?: string;
+  largeTitle?: string;
+  leftIcon?: Resources;
+  rightIcon?: Resources;
+  style?: StyleProp<ViewStyle>;
+  disabled?: boolean;
+  leftColor?: string;
+  rightColor?: string;
+  onPress?: () => void;
+}
+
+export const NavBar: React.FC<INavBar> = ({
+  title,
+  leftIcon,
+  rightIcon,
+  largeTitle,
+  onPress,
+  style,
+  rightColor,
+  leftColor,
+}) => {
+  return (
+    <Fragment>
+      <View style={CommonStyles.alignCenterJustifyBetweenRow}>
+        {leftIcon ? (
+          <Pressable
+            hitSlop={standardHitSlopSize}
+            style={({pressed}) => [{opacity: pressed ? 0.5 : 1}]}
+            onPress={onPress}>
+            <SvgImage source={leftIcon} color={leftColor} />
+          </Pressable>
+        ) : (
+          <View />
+        )}
+        {title ? (
+          <Text style={TypographyStyles.title3}>{title}</Text>
+        ) : (
+          <View />
+        )}
+        {rightIcon ? (
+          <Pressable
+            hitSlop={standardHitSlopSize}
+            style={({pressed}) => [{opacity: pressed ? 0.5 : 1}]}
+            onPress={onPress}>
+            <SvgImage source={rightIcon} color={rightColor} />
+          </Pressable>
+        ) : (
+          <View />
+        )}
+      </View>
+      {largeTitle ? (
+        <Text style={[TypographyStyles.title2, style]}>{largeTitle}</Text>
+      ) : null}
+    </Fragment>
+  );
+};
