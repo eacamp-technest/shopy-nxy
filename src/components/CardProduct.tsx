c246cd8516b9923a622fb81d951689e7bc063276
musayevnadir <msyv.nadir@gmail.com>
2024-06-04 22:11:49 +0400

    feat(card-component): created product position

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -21,34 +21,51 @@ export const CardProduct: React.FC<ICardProduct> = ({
   price,
 }) => {
   return (
-    <View style={styles.root}>
+    <View style={[styles.root, type === 'product' ? styles.product : null]}>
       <Image
         source={image}
-        style={[styles.imageNormal, type === 'save' ? styles.imageLarge : null]}
+        style={[
+          styles.imageNormal,
+          type === 'save' ? styles.imageMedium : null,
+          type === 'product' ? styles.imageLarge : null,
+        ]}
       />
-      <View style={[styles.list]}>
-        <Text numberOfLines={2}>{title}</Text>
-        {type === 'list' ? (
-          <Text>{`$${price}`}</Text>
+      <Fragment>
+        {type === 'product' ? (
+          <View>
+            <Text>{title}</Text>
+            <Text>{`$${price}`}</Text>
+            <Text>nike.com</Text>
+          </View>
         ) : (
-          <Fragment>
-            {type === 'add' ? (
-              <View style={[styles.main]}>
-                <Steppers type="transparent" size={'small'} count={0} />
-                <Text>{`$${price}`}</Text>
-              </View>
+          <View style={styles.list}>
+            <Text numberOfLines={2}>{title}</Text>
+            {type === 'list' ? (
+              <Text>{`$${price}`}</Text>
             ) : (
               <Fragment>
-                <Text>{`$${price}`}</Text>
-                <View style={[styles.main]}>
-                  <Text style={{paddingHorizontal: 16}}>Move to Bag</Text>
-                  <SvgImage isPressable={true} source={ImageResources.bell} />
-                </View>
+                {type === 'add' ? (
+                  <View style={[styles.main]}>
+                    <Steppers type="transparent" size={'small'} count={0} />
+                    <Text>{`$${price}`}</Text>
+                  </View>
+                ) : (
+                  <Fragment>
+                    <Text>{`$${price}`}</Text>
+                    <View style={[styles.main]}>
+                      <Text style={{paddingHorizontal: 16}}>Move to Bag</Text>
+                      <SvgImage
+                        isPressable={true}
+                        source={ImageResources.bell}
+                      />
+                    </View>
+                  </Fragment>
+                )}
               </Fragment>
             )}
-          </Fragment>
+          </View>
         )}
-      </View>
+      </Fragment>
     </View>
   );
 };
@@ -58,13 +75,22 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     gap: normalize('horizontal', 20),
   },
+  product: {
+    flexDirection: 'column',
+    gap: normalize('vertical', 12),
+  },
   imageNormal: {
     height: normalize('height', 78),
     width: normalize('width', 78),
   },
-  imageLarge: {
+  imageMedium: {
     height: normalize('height', 100),
     width: normalize('width', 100),
+  },
+  imageLarge: {
+    borderRadius: 8,
+    height: normalize('height', 156),
+    width: normalize('width', 141),
   },
   list: {
     flex: 1,
