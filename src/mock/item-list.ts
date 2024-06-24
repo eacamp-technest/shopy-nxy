842a49f9c86647058e543411dbf2489ccf4077f2
musayevnadir <msyv.nadir@gmail.com>
2024-06-24 14:48:26 +0400

    feat(list-man-screen): added main-tab and mock date

--- src/mock/item-list.ts
+++ src/mock/item-list.ts
@@ -46,3 +46,46 @@ export const woman: IMainTab[] = [
 ];
 
 // MAN
+
+export const man: IMainTab[] = [
+  {
+    id: 0,
+    title: 'Long T-Shirts',
+    leftIcon: require('../assets/vectors/chevronRight.svg'),
+  },
+  {
+    id: 1,
+    title: 'Premius T-Shirts',
+    leftIcon: require('../assets/vectors/chevronRight.svg'),
+  },
+  {
+    id: 2,
+    title: 'Pullover Hoodies',
+    leftIcon: require('../assets/vectors/chevronRight.svg'),
+  },
+  {
+    id: 3,
+    title: 'Pullovers',
+    leftIcon: require('../assets/vectors/chevronRight.svg'),
+  },
+  {
+    id: 4,
+    title: 'Shoes',
+    leftIcon: require('../assets/vectors/chevronRight.svg'),
+  },
+  {
+    id: 5,
+    title: 'Socks',
+    leftIcon: require('../assets/vectors/chevronRight.svg'),
+  },
+  {
+    id: 6,
+    title: 'Unisex Tank Tops',
+    leftIcon: require('../assets/vectors/chevronRight.svg'),
+  },
+  {
+    id: 7,
+    title: 'Zipped Hoodies',
+    leftIcon: require('../assets/vectors/chevronRight.svg'),
+  },
+];
