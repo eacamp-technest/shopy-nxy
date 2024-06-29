d1fc61bdaa62c6f5e8e3d93dfcf0f2f62a348a73
musayevnadir <msyv.nadir@gmail.com>
2024-06-29 16:30:10 +0400

    feat(permissions): added camera && media permissions

--- src/screens/Settings.Screen.tsx
+++ src/screens/Settings.Screen.tsx
@@ -1,5 +1,5 @@
-import React, {Fragment} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {Fragment, useEffect} from 'react';
+import {StyleSheet, ScrollView, Alert, Linking} from 'react-native';
 import {colors} from 'theme/colors';
 import {settings} from 'mock/settings';
 import {normalize} from 'theme/metrics';
@@ -8,8 +8,10 @@ import {MainTab} from 'components/MainTab';
 import {Divider} from 'components/Divider';
 import {useUserStoreActions} from 'store/user';
 import {StackRoutes, TabRoutes} from 'router/routes';
+import {ImageResources} from 'assets/VectorResources.g';
 import {SafeTopProvider} from 'containers/SafeTopProvider';
 import {NavigationParamList} from 'types/navigation.types';
+import {check, PERMISSIONS, RESULTS} from 'react-native-permissions';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 
 export const SettingsScreen: React.FC<
@@ -22,10 +24,40 @@ export const SettingsScreen: React.FC<
   const handleNextScreen = (id: number) => {
     navigation.navigate(screensArray[id]);
   };
+
+  const checkCameraPermission = async () => {
+    const checkCamera = await check(PERMISSIONS.IOS.CAMERA);
+    if (checkCamera === RESULTS.BLOCKED) {
+      Alert.alert(
+        'Camera permission is blocked',
+        'Please enable camera permission in settings',
+        [
+          {
+            text: 'Open settings',
+            onPress: Linking.openSettings,
+            isPreferred: true,
+          },
+          {
+            text: 'Cancel',
+            isPreferred: false,
+          },
+        ],
+      );
+    }
+  };
+
+  useEffect(() => {
+    checkCameraPermission();
+  }, []);
+
+  const handlePermissionMedia = () => {
+    console.log('Permissions media');
+  };
+
   return (
     <SafeTopProvider backColorSafeProvider={colors.white}>
-      <View>
-        <NavBar title={'SETTINGS'} styleTitle={colors.ink.base} />
+      <NavBar title={'SETTINGS'} styleTitle={colors.ink.base} />
+      <ScrollView showsVerticalScrollIndicator={false}>
         {settings.map(item => (
           <Fragment key={item.id}>
             {item.id === 4 ? <Divider height={'medium'} /> : null}
@@ -36,9 +68,27 @@ export const SettingsScreen: React.FC<
               leftIcon={item.leftIcon}
               rightIcon={item.rightIcon}
             />
+            {item.id === 5 ? (
+              <>
+                <MainTab
+                  style={styles.mainTab}
+                  title={'Permission camera'}
+                  rightIcon={item.rightIcon}
+                  leftIcon={ImageResources.camera}
+                  onPress={checkCameraPermission}
+                />
+                <MainTab
+                  style={styles.mainTab}
+                  title={'Permission media'}
+                  leftIcon={ImageResources.media}
+                  rightIcon={item.rightIcon}
+                  onPress={handlePermissionMedia}
+                />
+              </>
+            ) : null}
           </Fragment>
         ))}
-      </View>
+      </ScrollView>
     </SafeTopProvider>
   );
 };
