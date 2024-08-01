9f08dff2a768a284a541f016ec5a56a43c9fde8d
musayevnadir <msyv.nadir@gmail.com>
2024-08-01 21:58:24 +0400

    feat(slider): created initial styles slider-color component

--- src/components/SliderColor.tsx
+++ src/components/SliderColor.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import Slider from '@react-native-community/slider';
 import {colors} from 'theme/colors';
 import {normalize} from 'theme/metrics';
+import {TypographyStyles} from 'theme/typography';
+import MultiSlider from '@ptomasroos/react-native-multi-slider';
 
 export const SliderColor: React.FC = () => {
+  const [values, setValues] = useState([1, 5]);
+
   return (
     <View style={styles.root}>
-      <Slider
-        style={styles.slider}
-        minimumValue={0}
-        maximumValue={2}
-        minimumTrackTintColor={colors.skyLight}
-        maximumTrackTintColor={colors.primary.base}
-      />
+      <View style={styles.slider}>
+        <Text style={TypographyStyles.title3}>PRICE RANGE</Text>
+        <View style={styles.price}>
+          <Text style={TypographyStyles.RegularTightSemibold}>{'$69'}</Text>
+          <Text style={TypographyStyles.RegularTightSemibold}>{'$321'}</Text>
+        </View>
+        <MultiSlider
+          min={1}
+          max={2}
+          step={1}
+          values={values}
+          sliderLength={280}
+          onValuesChange={setValues}
+          trackStyle={styles.trackStyle}
+          markerStyle={styles.markerStyle}
+          selectedStyle={styles.selectedStyle}
+          unselectedStyle={styles.unselectedStyle}
+        />
+      </View>
     </View>
   );
 };
@@ -23,6 +38,27 @@ const styles = StyleSheet.create({
     paddingHorizontal: normalize('horizontal', 24),
   },
   slider: {
-    height: 2,
+    gap: normalize('vertical', 16),
+    // backgroundColor: 'green',
+  },
+  price: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  markerStyle: {
+    borderWidth: 0,
+    borderRadius: 100,
+    height: normalize('height', 32),
+    width: normalize('width', 32),
+    backgroundColor: colors.primary.base,
+  },
+  unselectedStyle: {
+    backgroundColor: colors.skyLight,
+  },
+  trackStyle: {
+    height: normalize('height', 2),
+  },
+  selectedStyle: {
+    backgroundColor: colors.primary.base,
   },
 });
--- src/screens/favoriteTabView/Filter.Screen.tsx
+++ src/screens/favoriteTabView/Filter.Screen.tsx
@@ -1,13 +1,18 @@
+import {ImageResources} from 'assets/VectorResources.g';
+import {Divider} from 'components/Divider';
+import {NavBar} from 'components/NavBar';
 import {SliderColor} from 'components/SliderColor';
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {SceneRendererProps} from 'react-native-tab-view';
 import {colors} from 'theme/colors';
+import {normalize} from 'theme/metrics';
 
 export const FilterScreen: React.FC<SceneRendererProps> = ({}) => {
   return (
     <View style={styles.root}>
       <SliderColor />
+      <Divider height={'large'} />
     </View>
   );
 };
@@ -15,6 +20,9 @@ export const FilterScreen: React.FC<SceneRendererProps> = ({}) => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
+    gap: normalize('vertical', 32),
+    borderStartColor: colors.white,
+    paddingTop: normalize('vertical', 24),
 
     backgroundColor: 'white',
   },
